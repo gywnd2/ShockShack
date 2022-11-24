f7a8d35f88332175623cc6ed31abc6f925039fcf
Hyojung Lee <leeexpert@cau.ac.kr>
2022-11-25 02:15:42 +0900

    Implementing google one tap login

--- client/app/src/main/java/com/udangtangtang/shockshack/MainActivity.kt
+++ client/app/src/main/java/com/udangtangtang/shockshack/MainActivity.kt
@@ -1,23 +1,70 @@
 package com.udangtangtang.shockshack
 
 import android.app.ProgressDialog.show
+import android.content.IntentSender
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
 import com.udangtangtang.shockshack.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+
+    // Google Sign-in
+    private lateinit var oneTapClient:SignInClient
+    private lateinit var signInRequest:BeginSignInRequest
+    private var REQ_ONE_TAP=100
+    private var TAG="MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Hide Action Bar
+        supportActionBar?.hide()
+
+        // Google Sign-in
+        // Configure One-tap login
+        oneTapClient=Identity.getSignInClient(this)
+        signInRequest=BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    .setServerClientId(getString(R.string.web_client_id))
+                    .setFilterByAuthorizedAccounts(true)
+                    .build())
+            .setAutoSelectEnabled(true)
+            .build()
+
+        // Display One-tap login UI
+        oneTapClient.beginSignIn(signInRequest)
+            .addOnSuccessListener(this) { result ->
+                try {
+                    startIntentSenderForResult(
+                        result.pendingIntent.intentSender, REQ_ONE_TAP,
+                        null, 0, 0, 0, null)
+                } catch (e: IntentSender.SendIntentException) {
+                    Log.e(TAG, "Couldn't start One Tap UI: ${e.localizedMessage}")
+                }
+            }
+            .addOnFailureListener(this) { e ->
+                // No saved credentials found. Launch the One Tap sign-up flow, or
+                // do nothing and continue presenting the signed-out UI.
+                Log.d(TAG, e.localizedMessage)
+            }
+
+        // Make simple toast
         binding.button.setOnClickListener() {
             Toast
                 .makeText(applicationContext, "Hello World!", Toast.LENGTH_SHORT)
                 .show()
         }
+
     }
 }
