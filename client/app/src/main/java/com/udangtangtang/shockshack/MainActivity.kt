48713692eef744561ab096ae3220b7a15bcc552b
Hyojung Lee <leeexpert@cau.ac.kr>
2022-11-25 01:28:34 +0900

    Test making simple toast

--- client/app/src/main/java/com/udangtangtang/shockshack/MainActivity.kt
+++ client/app/src/main/java/com/udangtangtang/shockshack/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.udangtangtang.shockshack
 
+import android.app.ProgressDialog.show
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.udangtangtang.shockshack.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener() {
+            Toast
+                .makeText(applicationContext, "Hello World!", Toast.LENGTH_SHORT)
+                .show()
+        }
     }
 }
