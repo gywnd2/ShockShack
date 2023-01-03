640c204b9010afbb401d488e3d541691098a2aa0
hyeonho <6190606@naver.com>
2023-01-03 23:41:13 +0900

    name-based-queueing

--- backend/src/main/java/com/udangtangtang/shockshack/domain/ChatRequest.java
+++ backend/src/main/java/com/udangtangtang/shockshack/domain/ChatRequest.java
@@ -1,4 +1,15 @@
 package com.udangtangtang.shockshack.domain;
 
-public record ChatRequest(String username) {
+public record ChatRequest(String sessionId, String username) {
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof ChatRequest) return username.equals(((ChatRequest) obj).username);
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return username.hashCode();
+    }
 }
